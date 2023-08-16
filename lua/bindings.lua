d60ca35eb7de2f891568799e04b65a257b085ba0
ItsNoellaHere <49508517+ItsNoellaHere@users.noreply.github.com>
2023-08-16 04:21:44 -0600

    add new bindings

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -1,7 +1,20 @@
 require("util")
 
 -- CHADtree binds
-MapKeyN("<leader>v", ":CHADopen<cr>", true)
+MapKeyN("<leader>v", vim.cmd.CHADopen, true)
 
 MapKeyN("<leader>T", ":tabnew<cr>:term<cr>i", true)
 MapKeyN("<Leader>fe", "zXzR", false)
+
+MapKeyN("<leader>ss", vim.cmd.noh, false)
+
+MapKeyV("J", ":m '>+1<CR>gv=gv")
+MapKeyV("K", ":m '<-2<CR>gv=gv")
+
+MapKey("x", "<leader>p", "\"_dP")
+
+MapKeyN("<leader>y", "\"+y")
+MapKeyV("<leader>y", "\"+y")
+
+MapKeyN("<leader>d", "\"_d")
+MapKeyV("<leader>d", "\"_d")
